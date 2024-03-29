414ee021e947fc2fe0f18cc0985600621232909e
Devanshu Jha <99939860+Devanshu254@users.noreply.github.com>
2024-03-29 13:21:58 +0530

    Create getter_setter.js
    getter and setter file is committed

---
+++ 29th March 2024/Web D/getter_setter.js
@@ -0,0 +1,19 @@
+class User {
+    constructor(name) {
+        // Invokes the setter.
+        this.name = name;
+    }
+    get name() {
+        return this._name;
+    }
+    set name(value) {
+        if(value.length < 4) {
+            alert("Name is too short.")
+            return;
+        }
+        this.name = value;
+    }
+}
+let user = new User("John");
+console.log(user.name);
+user = new User("");
