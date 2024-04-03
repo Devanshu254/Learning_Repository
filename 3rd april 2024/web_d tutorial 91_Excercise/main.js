c88253edbc82513d5476b551acfa962e1562a5bf
Devanshu Jha <99939860+Devanshu254@users.noreply.github.com>
2024-04-03 13:49:16 +0530

    Create main.js
    main.js file is created

---
+++ 3rd april 2024/web_d tutorial 91_Excercise/main.js
@@ -0,0 +1,10 @@
+// You have to write a nodejs program to clear cutter inside a directory and organize the contents of that directory into different folders.
+
+// Eg:
+// 1. Name.jpg, 2.name.png, 3. this.pdf, 4. harry.zip, 5. rohan.zip, 6. cat.jpg.
+// Jpg naam ka eak folder ban jayega aur name.jpg uske ander aa jayegi.
+// jpg/name.jpg,jpg/cat.jpg. png/png,pdf/..
+
+// There are two ways to solve this problem.
+// 1. See how many extensions are there within this folder. Then create the folder for that files and then insert files within that folder.
+// 2.
