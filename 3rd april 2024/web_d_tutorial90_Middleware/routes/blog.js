9876b0c8113993c2143868eaee21c9b99e45adc4
Devanshu Jha <99939860+Devanshu254@users.noreply.github.com>
2024-04-03 13:48:07 +0530

    Create blog.js
    blog.js file is updated

---
+++ 3rd april 2024/web_d_tutorial90_Middleware/routes/blog.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+
+// middleware that is specific to this router
+const timeLog = (req, res, next) => {
+  console.log("Time: ", Date.now());
+  next();
+};
+router.use(timeLog);
+
+// define the home page route
+router.get("/", (req, res) => {
+  res.send("Birds home page");
+});
+// define the about route
+router.get("/about", (req, res) => {
+  res.send("About birds");
+});
+
+module.exports = router;
