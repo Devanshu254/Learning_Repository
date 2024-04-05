94277624e2da38f7f0bf5c2a69d3c8dba16da8b9
Devanshu Jha <99939860+Devanshu254@users.noreply.github.com>
2024-04-05 09:12:35 +0530

    Update main.js
    main.js file is updated

--- 5th April 2024/web_dev_tutorial_98_ejs_in_express/main.js
+++ 5th April 2024/web_dev_tutorial_98_ejs_in_express/main.js
@@ -1,1 +1,48 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+// There are other view engines also, like pug, template engine handlebars. But we will go with ejs.
+
+// app.get("/", (req, res) => {
+//   let siteName = "Adidas"; // Maanlo ki hamne ye chheez database se nikaali hai.
+//   let searchText = "Search Now";
+//   // Suppose that above two values we have fetched from database. But now how will I put them within this template.
+//   res.sendFile("templates/index.html", { root: __dirname });
+// });
+
+app.get("/", (req, res) => {
+  let siteName = "Adidas"; // Maanlo ki hamne ye chheez database se nikaali hai.
+  let searchText = "Search Now";
+  let arr = [1, 54, 65];
+  res.render("index", {
+    siteName: siteName,
+    searchText: searchText,
+    arr,
+  });
+});
+
+// app.get("/blog/:slug", (req, res) => {
+//   let blogTitle = "Adidas why and when"; // Maanlo ki hamne ye chheez database se nikaali hai.
+//   let blogContent = "It is a very good brand.";
+//   res.sendFile("templates/blogpost.html", { root: __dirname });
+//   // We want that above two variables must be pushed within a template. But how ? For each blog I will not create blog.html.
+//   // The solution to this problem is template engine. Ejs - template engine and to use it we do npm install ejs.
+// });
+
+app.get("/blog/:slug", (req, res) => {
+  let blogTitle = "Adidas why and when"; // Maanlo ki hamne ye chheez database se nikaali hai.
+  let blogContent = "It is a very good brand.";
+  res.render("blogpost", {
+    // we do not need to write here views/index.ejs, we can directly write index.
+    blogTitle: blogTitle,
+    blogContent: blogContent,
+  });
+  // This means we have passed an object and we said that whatever we will get as blogTitle.
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
 
