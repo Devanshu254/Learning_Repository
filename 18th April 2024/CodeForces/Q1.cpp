88e2483bbc805d4ce4efde430a926bad2a3c3fee
Devanshu Jha <99939860+Devanshu254@users.noreply.github.com>
2024-04-19 00:42:14 +0530

    Update Q1.cpp
    Q1 is updated and comitted

--- 18th April 2024/CodeForces/Q1.cpp
+++ 18th April 2024/CodeForces/Q1.cpp
@@ -1,1 +1,22 @@
-
+// Q Link -> https://codeforces.com/problemset/problem/4/A
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int N;
+    cin >> N;
+    if (N == 2)
+    {
+        cout << "NO" << endl;
+        return 0;
+    }
+    if (N % 2 == 0)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
