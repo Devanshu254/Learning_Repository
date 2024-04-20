34888365149667fca7ffa9231a99856ad22caa97
Devanshu Jha <99939860+Devanshu254@users.noreply.github.com>
2024-04-20 18:45:55 +0530

    Create Q2.cpp
    Q2.cpp file is created and updated

---
+++ 20th April 2024/CodeForces/Q2.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int T;
+    cin>>T;
+    int result = 0;
+    while(T--) {
+        string str;
+        cin>>str;
+        if(str[0] == '+') {
+            result++;
+        }
+        if(str[0] == '-') {
+            result--;
+        }
+        if(str[2] == '+') {
+            result++;
+        }
+        if(str[2] == '-') {
+            result--;
+        }
+    }
+    cout<<result<<endl;
+    return 0;
+}
