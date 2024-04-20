c0cbe576557ed7f539b7ed0e385d2957c0ff426a
Devanshu Jha <99939860+Devanshu254@users.noreply.github.com>
2024-04-20 18:56:31 +0530

    Create Q3.cpp
    Q3 file is created and updated.

---
+++ 20th April 2024/CodeForces/Q3.cpp
@@ -0,0 +1,19 @@
+// Problem link -> https://codeforces.com/problemset/problem/158/A
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int N, K;
+    cin>>N>>K;
+    int arr[N];
+    int count = 0;
+    for(int i=0;i<N;++i) {
+        cin>>arr[i];
+    }
+    for(int i=0;i<N;++i) {
+        if(arr[i]>=arr[K-1] && arr[i]!=0) {
+            count++;
+        }
+    }
+    cout<<count<<endl;
+    return 0;
+}
