a2c13f5be720f955b4d32d18c28b85a7e1b94c5c
Devanshu Jha <99939860+Devanshu254@users.noreply.github.com>
2024-04-20 18:45:26 +0530

    Create Q1.cpp
    Q1. File Cpp is created and updated

---
+++ 20th April 2024/CodeForces/Q1.cpp
@@ -0,0 +1,18 @@
+// Problem Link -> https://codeforces.com/problemset/problem/231/A
+// Current Status -> Not Solved.
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int T;
+    cin>>T;
+    int result = 0;
+    while(T--) {
+        int p, v, t;
+        cin>>p>>v>>t;
+        if(p+v+t >=2) {
+            result++;
+        }
+    }
+    cout<<result<<endl;
+    return 0;
+}
