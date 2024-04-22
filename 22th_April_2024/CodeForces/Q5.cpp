1cdd3a5897aea96e4050833115eeb1c868542037
Devanshu Jha <99939860+Devanshu254@users.noreply.github.com>
2024-04-22 22:38:26 +0530

    Update Q5.cpp
    Q5 solution is updated

--- 22th_April_2024/CodeForces/Q5.cpp
+++ 22th_April_2024/CodeForces/Q5.cpp
@@ -1,1 +1,24 @@
 https://codeforces.com/problemset/problem/281/A?csrf_token=bb25a090796f19aab0697cce6f973f9b
+// https://codeforces.com/problemset/problem/281/A?csrf_token=bb25a090796f19aab0697cce6f973f9b
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    string str;
+    cin>>str;
+    if(str[0] >= 'a' && str[0] <= 'z') {
+        str[0] = str[0] - 'a' + 'A';
+    }
+    cout<<str<<endl;
+    return 0;
+}
+// We can also use toupper function.
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main() {
+//     string str;
+//     cin>>str;
+//     str[0] = toupper(str[0]); // Assign the uppercase version back to str[0]
+//     cout<<str<<endl;
+//     return 0;
+// }
+
