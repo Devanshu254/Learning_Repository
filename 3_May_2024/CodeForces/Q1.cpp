f86833615e9980da52cf5725960eefdbf44b936b
Devanshu Jha <99939860+Devanshu254@users.noreply.github.com>
2024-05-03 23:35:05 +0530

    Update Q1.cpp
    Q1 File is created and submitted

--- 3_May_2024/CodeForces/Q1.cpp
+++ 3_May_2024/CodeForces/Q1.cpp
@@ -1,1 +1,20 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    int result = INT_MIN;
+    if(n <= 5) {
+        cout<<1<<endl;
+    }else {
+        if(n % 5 == 0) {
+            result = abs(n/5);
+            cout<<result<<endl;
+        }
+        else {
+            result = (abs(n/5))+1;
+            cout<<result<<endl;
+        }
+    }
+    return 0;
+}
