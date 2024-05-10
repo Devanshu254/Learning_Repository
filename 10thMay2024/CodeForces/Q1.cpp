2db6ca5aedaac174d3ee9f4bb4ac531f99b4ac70
Devanshu Jha <99939860+Devanshu254@users.noreply.github.com>
2024-05-10 14:20:54 +0530

    Update Q1.cpp
    Q1.cpp is updated

--- 10thMay2024/CodeForces/Q1.cpp
+++ 10thMay2024/CodeForces/Q1.cpp
@@ -1,1 +1,26 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    string s;
+    cin>>s;
+    int isUpper = 0, isLower = 0;
+    for(char c: s) {
+        if(isupper(c)) {
+            isUpper++;
+        } else {
+            isLower++;
+        }
+    }
+    if(isUpper > isLower) {
+        for(int i=0;i<s.size();i++) {
+            s[i] = toupper(s[i]);
+        }
+        cout<<s<<endl;
+    }else {
+        for(int i=0;i<s.size();i++) {
+            s[i] = tolower(s[i]);
+        }
+        cout<<s<<endl;
+    }
+    return 0;
+}
