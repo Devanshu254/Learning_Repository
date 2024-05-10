97625ae8dff074c9e68b235781c19045f3039631
Devanshu Jha <99939860+Devanshu254@users.noreply.github.com>
2024-05-10 14:21:19 +0530

    Create Q2.cpp
    Q2.cpp file is updated

---
+++ 10thMay2024/CodeForces/Q2.cpp
@@ -0,0 +1,16 @@
+// https://codeforces.com/problemset/problem/977/A
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int n,k;
+    cin>>n>>k;
+    for(int i=0;i<k;i++) {
+        if(n%10 == 0) {
+            n = n / 10;
+        }else {
+            n  = n-1;
+        }
+    }
+    cout<<n<<endl;
+    return 0;
+}
