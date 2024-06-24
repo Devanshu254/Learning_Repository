aa3c35f8ae99d8e7013bdb75b48516910b08c8ef
Devanshu Jha <99939860+Devanshu254@users.noreply.github.com>
2024-06-24 22:10:07 +0530

    Update App.jsx

--- 24th June 2024/React JS/App.jsx
+++ 24th June 2024/React JS/App.jsx
@@ -2,3 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+function App() {
+  const [count, setCount] = useState(0)
+  // const [name, setName] = useState("Harry")
+  const [form, setForm] = useState({email: "", phone: ""})
