b86161b4b672cd57629c11a7298966f22bbb4952
Devanshu Jha <99939860+Devanshu254@users.noreply.github.com>
2024-06-24 22:11:01 +0530

    Update App.jsx

--- 24th June 2024/React JS/App.jsx
+++ 24th June 2024/React JS/App.jsx
@@ -16,3 +16,18 @@ const handleMouseOver = () => {
     setForm({...form, [e.target.name]:e.target.value});
     console.log(form)
   }
+  return (
+    <>
+      <div className = "button">
+        <button onClick={handleClick}>Click Me!</button>
+      </div>
+
+      <div className = "red" onMouseOver={handleMouseOver}>
+        I am a red div.
+      </div>
+
+      <input type="text" name='email' value={form.email} onChange={handleChange}/>
+      <input type="text" name='phone' value={form.phone} onChange={handleChange}/>
+    </>
+  )
+}
