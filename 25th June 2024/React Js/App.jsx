48491fe459595760d2f0c958c3c3812119ec39d9
Devanshu Jha <99939860+Devanshu254@users.noreply.github.com>
2024-06-25 22:05:18 +0530

    Update App.jsx

--- 25th June 2024/React Js/App.jsx
+++ 25th June 2024/React Js/App.jsx
@@ -2,3 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Navbar from './components/Navbar'
+function App() {
+  const [todo, setTodo] = useState("")
+  const [todos, setTodos] = useState([])
