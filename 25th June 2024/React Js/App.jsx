aee45bc4419b0d37f7b35e6f62446e1875bc2673
Devanshu Jha <99939860+Devanshu254@users.noreply.github.com>
2024-06-25 22:06:59 +0530

    Update App.jsx

--- 25th June 2024/React Js/App.jsx
+++ 25th June 2024/React Js/App.jsx
@@ -21,4 +21,10 @@ function App() {
   return (
     <>
     <Navbar/>
+      <div className='container mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[80vh]'>
+        <div className='addTodo my-5'>
+          <h2 className='text-lg font-bold'>Add a Todo</h2>
+          <input onChange = {handleChange} value = {todo} type="text" className='w-1/2'/>
+          <button onClick={handleAdd} className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-6">Add</button>
+        </div>
 
