9f729a78509c8cfaba117292227ac9e67a2a2f01
Devanshu Jha <99939860+Devanshu254@users.noreply.github.com>
2024-06-25 22:10:38 +0530

    Update Navbar.jsx

--- 25th June 2024/React Js/Navbar.jsx
+++ 25th June 2024/React Js/Navbar.jsx
@@ -1,2 +1,13 @@
 import React from 'react'
 const Navbar = () => {
+  return (
+        <nav className='flex justify-between bg-slate-700 text-white py-2'>
+            <div className="logo">
+                <span className='font-bold text-xl mx-8'>iTask</span>
+            </div>
+            <ul className='flex gap-8 mx-9'>
+                <li className="cursor-pointer hover:font-bold transition-all duration-150">Home</li>
+                <li className="cursor-pointer hover:font-bold transition-all duration-150">Your_Tasks</li>
+            </ul>
+        </nav>
+    )
